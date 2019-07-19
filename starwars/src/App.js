7af3a9de16f3045ba31549b1227356af29398c53
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-07-19 10:46:01 -0600

    Card Components rendered, adding styled components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import CharacterCard from "./components/CharacterCard"
-// import styled from "styled-components";
-import './App.css';
+import styled from "styled-components";
+
+const StyledAppContainer = styled.div`
+  text-align: center;
+  max-width: 1300px;
+  width: 100%;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  margin-bottom: 50px;
+`;
+
+const StyledHeader = styled.h1`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -18,23 +38,22 @@ const App = () => {
     axios.get('https://swapi.co/api/people/')
     .then((response) => {
       console.log('Data Received', response.data.results);
-      setCharacterData(response.data.results.map((character) => {
-        return character
-      }))
+      setCharacterData(response.data.results)
     })
     .catch((error) => {
       console.log('Data Not Available')
     })
   }, [])
 
-  console.log(characterData);
-
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-      <CharacterCard name={}
-    </div>
-  );
+    <StyledAppContainer>
+    {characterData.map(character =>
+    <CharacterCard name={character.name} birthyear={character.birth_year} 
+      gender={character.gender} haircolor={character.hair_color} eyecolor={character.eye_color}
+      height={character.height} mass={character.mass}/>
+    )}
+    </StyledAppContainer>
+  )
 }
 
 export default App;
--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,8 +1,21 @@
 import React from "react"
+import styled from "styled-components";
+
+const StyledCard = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  max-width: 220px;
+  width: 17%;
+  height: 420px;
+  padding: 1%;
+  margin: 0.5%;
+  background-color: tan;
+  opacity: 0.5;
+`;
 
 const CharacterCard = props => {
     return (
-        <div>
+        <StyledCard>
             <h2>{props.name}</h2>
             <h3>Character Info</h3>
             <p>Birth Year: {props.birthyear}</p>
@@ -11,7 +24,7 @@ const CharacterCard = props => {
             <p>Eye Color: {props.eyecolor}</p>
             <p>Height: {props.height}</p>
             <p>Mass: {props.mass}</p>
-        </div>
+        </StyledCard>
     )
 }
 
